bcb428dafc08f95b2c7fd57afe976034712d241b
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-08-17 08:41:45 +0200

    Update setup script to add language regions to readme

--- setup.sh
+++ setup.sh
@@ -7,5 +7,10 @@ else
   mkdir Seminarski/$1/
   echo >> Seminarski/$1/README.md
 
+  echo "# Bosanski, hrvatski i srpski jezik" >> Seminarski/$1/README.md
+  echo " " >> Seminarski/$1/README.md
+  echo "# Engleski jezik" >> Seminarski/$1/README.md
+  echo " " >> Seminarski/$1/README.md
+
   echo "* [$1](./Seminarski/$1/README.md)" >> README.md
 fi
